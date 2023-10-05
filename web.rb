5d81863f49aec516537106c9847a2de0f7612a47
Charlotte <charlotte@lottia.net>
2023-10-05 20:41:13 +1100

    web: update.

--- web.rb
+++ web.rb
@@ -4,13 +4,14 @@ SPIDER = spider do
   free(:i).qsp("https://google.com/search?hl=en&tbm=isch", :q)
   free(:sw).replace("https://en.wiktionary.org/wiki/{query}#Spanish", space: "_")
   free(:auslan).replace("https://find.auslan.fyi/search?query={query}")
-  free(:enes).replace("https://translate.google.com/?source=osdd&sl=en&tl=es&text={query}&op=translate")
-  free(:esen).replace("https://translate.google.com/?source=osdd&sl=es&tl=en&text={query}&op=translate")
+
+  %w(en es ru).permutation(2).each do |sl, tl|
+    free(:"#{sl}#{tl}").replace("https://translate.google.com/?source=osdd&sl=#{sl}&tl=#{tl}&text={query}&op=translate")
+  end
 
   static(:adc).redirect("https://adc.hrzn.ee")
   static(:ynab).redirect("https://app.youneedabudget.com/")
   static(:ing).redirect("https://www.ing.com.au/securebanking/")
-  static(:"28d").redirect("https://servicecentre.latitudefinancial.com.au/login")
 
   static(:hi, :hello, :hey, :heya, :chewwo, :yawonk, :hola, :howdy).message(<<~HTML)
    Chewwo!!!! <span class='bunnywave'></span>
