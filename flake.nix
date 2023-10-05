995d028e89a7711cda7d8c86a430ad1283e12a0d
Charlotte <charlotte@lottia.net>
2023-10-05 21:08:40 +1100

    flake: add Darwin service.

--- flake.nix
+++ flake.nix
@@ -7,24 +7,84 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
       ruby = pkgs.ruby;
+
+      env = pkgs.bundlerEnv {
+        name = "comenzar-bundler-env";
+        inherit ruby;
+        gemfile = ./Gemfile;
+        lockfile = ./Gemfile.lock;
+        gemset = import ./gemset.nix;
+      };
     in {
       formatter = pkgs.alejandra;
 
-      devShells.default = let
-        env = pkgs.bundlerEnv {
-          name = "comenzar-bundler-env";
-          inherit ruby;
-          gemfile = ./Gemfile;
-          lockfile = ./Gemfile.lock;
-          gemset = import ./gemset.nix;
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "comenzar";
+        version = "0.0.1";
+
+        src = ./.;
+
+        nativeBuildInputs = [pkgs.makeWrapper];
+
+        buildInputs = [env];
+
+        # I'm sure there's a better way to get the GEM_PATH out here.
+        # Right?
+        installPhase = ''
+          mkdir -p $out
+          cp -r * $out/
+          wrapProgram $out/bin/comenzar \
+            --prefix PATH : ${pkgs.lib.makeBinPath [env]} \
+            --prefix GEM_PATH : ${env}/lib/ruby/gems/3.1.0
+        '';
+      };
+
+      devShells.default = pkgs.mkShell {
+        name = "comenzar";
+        buildInputs = [
+          ruby
+          env
+        ];
+      };
+
+      darwinModules.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: let
+        cfg = config.services.comenzar;
+        inherit (lib) mkIf mkEnableOption mkOption types;
+      in {
+        options.services.comenzar = {
+          enable = mkEnableOption "Enable the comenzar service";
+
+          package = mkOption {
+            type = types.package;
+            default = self.packages.${system}.default;
+            description = "Package to use for comenzar (defaults to this flake's).";
+          };
+
+          logFile = mkOption {
+            type = types.nullOr types.path;
+            default = null;
+            description = "The logfile to use for the comenzar service.";
+          };
         };
-      in
-        pkgs.mkShell {
-          name = "comenzar";
-          buildInputs = [
-            ruby
-            env
-          ];
+
+        config = mkIf (cfg.enable) {
+          launchd.user.agents.comenzar = {
+            serviceConfig = {
+              ProgramArguments = ["${cfg.package}/bin/comenzar"];
+              KeepAlive = true;
+              RunAtLoad = true;
+              ProcessType = "Background";
+              StandardOutPath = cfg.logFile;
+              StandardErrorPath = cfg.logFile;
+              EnvironmentVariables = {};
+            };
+          };
         };
+      };
     });
 }
