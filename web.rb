5fbdccac7570b4c0acf9fa157d4515a37c0fc5af
Charlotte <charlotte@lottia.net>
2023-10-25 13:36:00 +1100

    pupdate.

--- web.rb
+++ web.rb
@@ -4,15 +4,16 @@ SPIDER = spider do
   free(:i).qsp("https://google.com/search?hl=en&tbm=isch", :q)
   free(:sw).replace("https://en.wiktionary.org/wiki/{query}#Spanish", space: "_")
   free(:rw).replace("https://en.wiktionary.org/wiki/{query}#Russian", space: "_")
+  free(:ew).replace("https://en.wiktionary.org/wiki/{query}#Estonian", space: "_")
   free(:auslan).replace("https://find.auslan.fyi/search?query={query}")
 
-  %w(en es ru ja).permutation(2).each do |sl, tl|
+  %w(en es ru ja et nl de).permutation(2).each do |sl, tl|
     free(:"#{sl}#{tl}").replace("https://translate.google.com/?source=osdd&sl=#{sl}&tl=#{tl}&text={query}&op=translate")
   end
 
-  static(:adc).redirect("https://adc.hrzn.ee")
-  static(:ynab).redirect("https://app.youneedabudget.com/")
+  static(:ynab).redirect("https://app.ynab.com/")
   static(:ing).redirect("https://www.ing.com.au/securebanking/")
+  static(:lhv).redirect("https://www.lhv.ee/ibank/cf/portfolio/view?vi=0")
 
   static(:hi, :hello, :hey, :heya, :chewwo, :yawonk, :hola, :howdy).message(<<~HTML)
    Chewwo!!!! <span class='bunnywave'></span>
